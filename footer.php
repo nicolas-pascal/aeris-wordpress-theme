df9811f0b6a254a57d7d49373ec6b124427a0c48
Pierre Vert <vert@obs-mip.fr>
2017-01-09 17:55:10 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package aeris
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-aeris' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'theme-aeris' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'theme-aeris' ), 'theme-aeris', '<a href="https://automattic.com/" rel="designer">Pierre VERT</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
