f690142b7e7d8552c54cdafe2eddc2b1d69f41a3
Pierre Vert <vert@obs-mip.fr>
2017-01-10 17:49:00 +0100

    ajout custom nav

---
+++ inc/custom-nav.php
@@ -0,0 +1,23 @@
+<?php
+/******************************************************************
+* MENU FRONT END
+*
+* fonctions affectant les menus en front end
+* register_menu_location() -> Ajoute des emplacements de menu
+*/
+
+
+/** 
+ * Ajout d'emplacements de menu
+*/
+function theme_aeris_register_menu_location() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
+}
+add_action( 'init', 'theme_aeris_register_menu_location' );
+
+?>
