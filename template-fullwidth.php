0b7f4bc2c1c3209b027a913c041495a616d388d1
Pierre Vert <vert@obs-mip.fr>
2017-02-08 09:15:11 +0100

    template fullwidth

---
+++ template-fullwidth.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Page entière
+*/
+get_header(); 
+
+while ( have_posts() ) : the_post();
+
+	get_template_part( 'template-parts/header-content', 'page' );
+?>
+
+	<div id="content-area" class="wrapper full">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			
+			?>
+		
+		</main><!-- #main -->
+	</div><!-- #content-area -->
+
+<?php
+endwhile; // End of the loop.
+// get_sidebar();
+get_footer();
