fc50e910088b3723c80b416e46c3ea7bc1145f14
Pierre Vert <vert@obs-mip.fr>
2017-06-16 15:37:00 +0200

    template homepage custom

--- inc/custom-widget.php
+++ inc/custom-widget.php
@@ -33,6 +33,57 @@ function wpaeris_custom_sidebars() {
     );
     register_sidebar( $args );
 
+
+    /**** HOMEPAGE AREA  *****/
+
+    $args = array(
+        'id'            => 'homepage-top-widget-area',
+        'class'         => 'homepage-top-widget-area',
+        'name'          => __( 'homepage-top', 'text_domain' ),
+        'description'   => esc_html__( 'Ajouter des widgets ici.', 'theme-aeris' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    );
+    register_sidebar( $args );
+
+    $args = array(
+        'id'            => 'homepage-mdleft-widget-area',
+        'class'         => 'homepage-mdleft-widget-area',
+        'name'          => __( 'homepage milieu gauche', 'text_domain' ),
+        'description'   => esc_html__( 'Ajouter des widgets ici.', 'theme-aeris' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    );
+    register_sidebar( $args );
+
+        $args = array(
+        'id'            => 'homepage-mdright-widget-area',
+        'class'         => 'homepage-mdright-widget-area',
+        'name'          => __( 'homepage milieu droit', 'text_domain' ),
+        'description'   => esc_html__( 'Ajouter des widgets ici.', 'theme-aeris' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    );
+    register_sidebar( $args );
+
+    $args = array(
+        'id'            => 'homepage-footer-widget-area',
+        'class'         => 'homepage-footer-widget-area',
+        'name'          => __( 'homepage pre footer', 'text_domain' ),
+        'description'   => esc_html__( 'Ajouter des widgets ici.', 'theme-aeris' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    );
+    register_sidebar( $args );
+
 }
 add_action( 'widgets_init', 'wpaeris_custom_sidebars' );
 
