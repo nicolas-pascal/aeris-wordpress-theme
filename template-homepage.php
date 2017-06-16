e334c479d5bde07d6f1eb5870a14ce5803bd56b0
Pierre Vert <vert@obs-mip.fr>
2017-06-16 17:31:52 +0200

    correctifs homepage + button

--- template-homepage.php
+++ template-homepage.php
@@ -3,11 +3,28 @@
 Template Name: Homepage custom
 */
 get_header(); 
+// Description (slogan)
+$description = get_bloginfo( 'description', 'display' );
 
 while ( have_posts() ) : the_post();
 
-	get_template_part( 'template-parts/header-content', 'page' );
+
 ?>
+	<div id="breadcrumbs">
+		<?php if (function_exists('the_breadcrumb')) the_breadcrumb(); ?>
+	</div>
+	
+	<div class="site-branding" style="background-image:url(<?php header_image()?>);">
+		<div>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php
+			if ( $description || is_customize_preview() ) { ?>
+			<p class="site-description"><?php echo $description; ?></p>
+				<?php
+			}
+			?>
+		</div>
+	</div><!-- .site-branding -->
 
 	<div id="content-area" class="wrapper default">
 		<main id="main" class="site-main" role="main">
@@ -21,46 +38,56 @@ while ( have_posts() ) : the_post();
 				// endif;
 			
 			?>
-            <section role="homepage-top-widget-area">
             <?php 
-                if ( ! is_active_sidebar( 'homepage-top-widget-area' ) ) {
-                    return;
-                }
+            if ( is_active_sidebar( 'homepage-top-widget-area' ) ) {
                 ?>
-                    <?php dynamic_sidebar( 'homepage-top-widget-area' ); ?>
-
-         
+            <section role="homepage-top-widget-area">    
+                <?php
+                dynamic_sidebar( 'homepage-top-widget-area' );
+                ?>
             </section>
+            <?php
+            }
+            ?>
+            
             <div role="homepage-md">
-                <section role="homepage-mdleft-widget-area">
                 <?php 
-                    if ( ! is_active_sidebar( 'homepage-mdleft-widget-area' ) ) {
-                        return;
-                    }
+                if ( is_active_sidebar( 'homepage-mdleft-widget-area' ) ) {
                     ?>
-                        <?php dynamic_sidebar( 'homepage-mdleft-widget-area' ); ?>
-
-                </section>
-
-            <section role="homepage-mdright-widget-area">
-                <?php 
-                    if ( ! is_active_sidebar( 'homepage-mdright-widget-area' ) ) {
-                        return;
-                    }
+                <section role="homepage-mdleft-widget-area">    
+                    <?php
+                    dynamic_sidebar( 'homepage-mdleft-widget-area' );
                     ?>
-                        <?php dynamic_sidebar( 'homepage-mdright-widget-area' ); ?>
-
                 </section>
-            </div>
-           <section role="homepage-footer-widget-area">
-            <?php 
-                if ( ! is_active_sidebar( 'homepage-footer-widget-area' ) ) {
-                    return;
+                <?php
                 }
                 ?>
-                    <?php dynamic_sidebar( 'homepage-footer-widget-area' ); ?>
 
+                <?php 
+                if ( is_active_sidebar( 'homepage-mdright-widget-area' ) ) {
+                    ?>
+                <section role="homepage-mdright-widget-area">    
+                    <?php
+                    dynamic_sidebar( 'homepage-mdright-widget-area' );
+                    ?>
+                </section>
+                <?php
+                }
+                ?>            
+            </div>
+            <?php 
+            if ( is_active_sidebar( 'homepage-footer-widget-area' ) ) {
+                ?>
+            <section role="homepage-footer-widget-area">    
+                <?php
+                dynamic_sidebar( 'homepage-footer-widget-area' );
+                ?>
             </section>
+            <?php
+            }
+            ?>
+
+            
 		
 		</main><!-- #main -->
 		
