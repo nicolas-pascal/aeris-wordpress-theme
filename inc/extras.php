019b84d68ed64b587e79d9bc25dd9091038a1866
Pierre Vert <pierr65@gmail.com>
2020-05-14 11:16:13 +0200

    redirect after login, and CSS list related content (for Theia)

--- inc/extras.php
+++ inc/extras.php
@@ -300,3 +300,10 @@ function theme_aeris_hide_editor() {
   // } 
 }
 
+/******************************************************************
+* REDIRECT AFTER LOGIN
+*/
+function theme_aeris_login_redirect( $redirect_to, $request, $user ){
+  return home_url();
+}
+add_filter( 'theme_aeris_login_redirect', 'login_redirect', 10, 3 );
